4c1a1ab0f1cdaa8febfb6ffca90d567611bc1de9
E Ek <eek.datasc@gmail.com>
2025-11-28 09:11:46 +0000

    Filtering Data

--- init/create_db.sql
+++ init/create_db.sql
@@ -342,3 +342,51 @@ CREATE TABLE order_menu_item(
     FOREIGN KEY (menu_item_id) REFERENCES menu_item(menu_item_id) 
 ); 
 
+CREATE DATABASE IF NOT EXISTS Lucky_Shrub;
+
+USE Lucky_Shrub; 
+
+
+CREATE TABLE Orders (
+    OrderID INT NOT NULL PRIMARY KEY, 
+    ClientID VARCHAR(10),  
+    ProductID VARCHAR(10),  
+    Quantity   INT, 
+    Cost DECIMAL(6,2)
+); 
+
+INSERT INTO Orders (OrderID, ClientID, ProductID , Quantity, Cost) VALUES (1, "Cl1", "P1", 10, 500), (2, "Cl2", "P2", 5, 100), (3, "Cl3", "P3", 20, 800), (4, "Cl4", "P4", 15, 150), (5, "Cl3", "P3", 10, 450), (6, "Cl2", "P2", 5, 800), (7, "Cl1", "P4", 22, 1200), (8, "Cl3", "P1", 15, 150), (9, "Cl1", "P1", 10, 500), (10, "Cl2", "P2", 5, 100); 
+
+-- Task 1: Write a SQL statement to print all records of orders where the cost is not $250 or more.
+-- The expected output result should be the same as the following screenshot 
+-- (assuming that you have populated the Orders table with the same data set)
+SELECT
+ * 
+FROM Orders
+WHERE NOT (cost > 250);
+;
+
+
+-- Task 2: Write a SQL statement to print all records of orders where the cost is between $50 and $750.
+-- The expected output result should be the same as the following screenshot 
+-- (assuming that you have populated the orders table with the same data set)
+SELECT * FROM Orders
+WHERE cost BETWEEN 50 AND 750;
+
+
+-- Task 3: Write a SQL statement to print all records of orders that have been placed by the client with the id of Cl3 and where the cost of the order is more than $100.
+-- The expected output result should be the same as the following screenshot 
+-- (assuming that you have populated the Orders table with the same data set)
+SELECT * FROM Orders
+WHERE clientID = 'Cl3'
+AND cost > 100;
+
+
+-- Task 4: Write a SQL statement to print all records of orders that have a product id of p1 or p2 and the order quantity is more than 2.
+-- The expected output result should be the same as the following screenshot 
+-- (assuming that you have populated the Orders table with the same data set)
+SELECT * FROM Orders
+WHERE productID IN ('P1', 'P2')
+AND quantity > 2;
+
+
