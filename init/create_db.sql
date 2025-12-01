485d04451b345caa204d92226f69f8cadc60d824
E Ek <eek.datasc@gmail.com>
2025-12-01 08:39:28 +0000

    Working With Subquery

--- init/create_db.sql
+++ init/create_db.sql
@@ -604,3 +604,201 @@ SHOW COLUMNS FROM Staff;
 ALTER TABLE Staff DROP COLUMN PhoneNumber;
 
 SHOW COLUMNS FROM Staff;
+
+CREATE DATABASE IF NOT EXISTS little_lemon; 
+
+USE little_lemon;
+
+CREATE TABLE Customers (
+    CustomerID INT NOT NULL PRIMARY KEY,
+    FullName VARCHAR(100) NOT NULL,
+    PhoneNumber INT NOT NULL UNIQUE
+);
+
+SHOW COLUMNS FROM Customers;
+
+CREATE TABLE Bookings (
+    BookingID INT NOT NULL PRIMARY KEY,
+    BookingDate DATE NOT NULL,
+    TableNumber INT NOT NULL,
+    NumberOfGuest INT NOT NULL,
+    CustomerID INT NOT NULL,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+SHOW COLUMNS FROM Bookings;
+
+--  a new table in the Little Lemon restaurant database to store information about customers' orders.
+--  The new “Food Orders” table includes three columns:
+-- Order ID,
+-- Quantity, 
+-- Order_Status
+-- and Cost 
+
+CREATE TABLE FoodOrders (
+    OrderID INT, 
+    Quantity INT, 
+    Order_Status VARCHAR(15), 
+    Cost Decimal(4,2)
+);
+
+--  View the current table structure by typing:
+SHOW COLUMNS FROM FoodOrders;
+
+-- Task 1: Use the ALTER TABLE statement with MODIFY command to make the OrderID the primary key of the 'FoodOrders' table. 
+ALTER TABLE FoodOrders MODIFY OrderID INT PRIMARY KEY;
+SHOW COLUMNS FROM FoodOrders;
+
+-- Task 2: Apply the NOT NULL constraint to the quantity and cost columns.
+ALTER TABLE FoodOrders MODIFY Quantity INT NOT NULL;
+SHOW COLUMNS FROM FoodOrders;
+
+-- Task 3: Create two new columns, OrderDate with a DATE datatype and CustomerID with an INT datatype. 
+-- Declare both must as NOT NULL. Declare the CustomerID as a foreign key in the FoodOrders table to 
+-- reference the CustomerID column existing in the Customers table.
+ALTER TABLE FoodOrders ADD COLUMN OrderDate DATE, ADD COLUMN CustomerID INT;
+
+SHOW COLUMNS FROM FoodOrders;
+
+ALTER TABLE FoodOrders ADD  FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID);
+SHOW COLUMNS FROM FoodOrders;
+
+-- Task 4: Use the DROP command with ALTER statement to delete the OrderDate column from the 'FoodOrder' table. 
+-- Task 4: Use the DROP command with ALTER statement to delete the OrderDate column from the 'FoodOrder' table. 
+ALTER TABLE FoodOrders DROP COLUMN OrderDate;
+SHOW COLUMNS FROM FoodOrders;
+
+-- Task 5: Use the CHANGE command with ALTER statement to rename the column Order_Status in the OrderStatus table to DeliveryStatus. 
+ALTER TABLE FoodOrders CHANGE Order_status DeliveryStatus VARCHAR(15);
+SHOW COLUMNS FROM FoodOrders;
+
+-- Task 6: Use the RENAME command with ALTER statement to change the table name from OrderStatus to OrderDeliveryStatus.
+ALTER TABLE FoodOrders RENAME COLUMN DeliveryStatus TO OrderDeliveryStatus;
+SHOW COLUMNS FROM FoodOrders;
+
+CREATE DATABASE littlelemon_db;
+USE littlelemon_db;
+
+CREATE TABLE MenuItems ( 
+  ItemID INT, 
+  Name VARCHAR(200), 
+  Type VARCHAR(100), 
+  Price INT, 
+  PRIMARY KEY (ItemID) 
+); 
+
+CREATE TABLE Menus ( 
+  MenuID INT, 
+  ItemID INT, 
+  Cuisine VARCHAR(100), 
+  PRIMARY KEY (MenuID,ItemID)
+); 
+
+CREATE TABLE Bookings ( 
+  BookingID INT, 
+  TableNo INT, 
+  GuestFirstName VARCHAR(100), 
+  GuestLastName VARCHAR(100), 
+  BookingSlot TIME, 
+  EmployeeID INT, 
+  PRIMARY KEY (BookingID) 
+);  
+
+
+CREATE TABLE TableOrders ( 
+  OrderID INT, 
+  TableNo INT, 
+  MenuID INT, 
+  BookingID INT, 
+  BillAmount INT, 
+  Quantity INT, 
+  PRIMARY KEY (OrderID,TableNo) 
+);  
+
+
+INSERT INTO MenuItems VALUES(1,'Olives','Starters', 5), 
+(2,'Flatbread','Starters', 5),
+(3, 'Minestrone', 'Starters', 8), 
+(4, 'Tomato bread','Starters', 8), 
+(5, 'Falafel', 'Starters', 7), 
+(6, 'Hummus', 'Starters', 5), 
+(7, 'Greek salad', 'Main Courses', 15), 
+(8, 'Bean soup', 'Main Courses', 12), 
+(9, 'Pizza', 'Main Courses', 15), 
+(10,'Greek yoghurt','Desserts', 7), 
+(11, 'Ice cream', 'Desserts', 6),
+(12, 'Cheesecake', 'Desserts', 4), 
+(13, 'Athens White wine', 'Drinks', 25), 
+(14, 'Corfu Red Wine', 'Drinks', 30), 
+(15, 'Turkish Coffee', 'Drinks', 10), 
+(16, 'Turkish Coffee', 'Drinks', 10), 
+(17, 'Kabasa', 'Main Courses', 17);
+
+INSERT INTO Menus VALUES(1, 1, 'Greek'), 
+(1, 7, 'Greek'), 
+(1, 10, 'Greek'), 
+(1, 13, 'Greek'), 
+(2, 3, 'Italian'), 
+(2, 9, 'Italian'), 
+(2, 12, 'Italian'), 
+(2, 15, 'Italian'), 
+(3, 5, 'Turkish'), 
+(3, 17, 'Turkish'), 
+(3, 11, 'Turkish'), 
+(3, 16, 'Turkish');
+
+INSERT INTO Bookings VALUES(1,12,'Anna','Iversen','19:00:00',1),  
+(2, 12, 'Joakim', 'Iversen', '19:00:00', 1), 
+(3, 19, 'Vanessa', 'McCarthy', '15:00:00', 3), 
+(4, 15, 'Marcos', 'Romero', '17:30:00', 4), 
+(5, 5, 'Hiroki', 'Yamane', '18:30:00', 2),
+(6, 8, 'Diana', 'Pinto', '20:00:00', 5); 
+
+INSERT INTO TableOrders VALUES(1, 12, 1, 1, 2, 86), 
+(2, 19, 2, 2, 1, 37), 
+(3, 15, 2, 3, 1, 37), 
+(4, 5, 3, 4, 1, 40), 
+(5, 8, 1, 5, 1, 43);
+
+-- Task 1: Write a SQL SELECT query to find all bookings that are due after the booking of the guest ‘Vanessa McCarthy’.
+SELECT * FROM Bookings
+WHERE 1 = 1
+AND BookingSlot > (
+    SELECT BookingSlot FROM Bookings
+    WHERE 1 = 1
+    AND GuestFirstName = "Vanessa"
+);
+
+-- Task 2: Write a SQL SELECT query to find the menu items that are more expensive than all the 'Starters' and 'Desserts' menu item types.
+SELECT * FROM MenuItems
+WHERE 1 = 1
+AND Price > ALL (
+    SELECT Price FROM MenuItems
+    WHERE 1 = 1
+    AND Type IN ('Starters', 'Desserts')
+);
+
+-- Task 3: Write a SQL SELECT query to find the menu items that costs the same as the starter menu items that are Italian cuisine.
+SELECT * FROM MenuItems
+WHERE 1 = 1 
+AND Price =
+(
+    SELECT Price FROM MenuItems 
+    INNER JOIN Menus ON MenuItems.ItemID = Menus.ItemID
+    WHERE 1 = 1
+    AND Menus.Cuisine = 'Italian'
+    AND MenuItems.Type = 'Starters'
+
+);
+-- Task 4: Write a SQL SELECT query to find the menu items that were not ordered by the guests who placed bookings.
+SELECT * FROM MenuItems
+WHERE 1 = 1
+AND ItemID NOT IN 
+    (
+        SELECT Menus.ItemID FROM TableOrders
+        INNER JOIN Menus ON TableOrders.MenuID = Menus.MenuID   
+    );
+
+
+SELECT * FROM MenuItems 
+WHERE NOT EXISTS (SELECT * FROM TableOrders, Menus WHERE TableOrders.MenuID = Menus.MenuID AND Menus.ItemID = MenuItems.ItemID); 
